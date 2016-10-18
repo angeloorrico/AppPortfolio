3a4f075f083e5d21788736311f226195b75c5400
angeloorrico <a.orrico@gmail.com>
2016-10-18 15:57:47 -0300

    Using Snakcnar instead of Toast to show app messages.

--- app/src/main/java/br/com/angeloorrico/myappportfolio/MainActivity.java
+++ app/src/main/java/br/com/angeloorrico/myappportfolio/MainActivity.java
@@ -1,9 +1,10 @@
 package br.com.angeloorrico.myappportfolio;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,30 +15,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openApp(View view) {
-        String selectedApp = "";
-
-        switch (view.getId()) {
-            case R.id.bt_popular_movies:
-                selectedApp = getString(R.string.app_popular_movies);
-                break;
-            case R.id.bt_stock_hawk:
-                selectedApp = getString(R.string.app_stock_hawk);
-                break;
-            case R.id.bt_build_it_bigger:
-                selectedApp = getString(R.string.app_build_it_bigger);
-                break;
-            case R.id.bt_make_your_app_material:
-                selectedApp = getString(R.string.app_make_your_app_material);
-                break;
-            case R.id.bt_go_ubiquitous:
-                selectedApp = getString(R.string.app_go_ubiquitous);
-                break;
-            case R.id.bt_capstone:
-                selectedApp = getString(R.string.app_capstone);
-                break;
-        }
-
-        Toast.makeText(this, selectedApp + " " + getString(R.string.under_development),
-                Toast.LENGTH_SHORT).show();
+        Button bt = (Button) view;
+        Snackbar.make(view, String.format(getString(R.string.under_development), bt.getText()),
+                Snackbar.LENGTH_SHORT).show();
     }
+
 }
